3ecdc02d7bce4497ddc23477f3fef2b9c1b346a7
Marco Meyer-Conde　　　  メイヤーマコト <37673095+meiyasan@users.noreply.github.com>
2025-07-18 10:57:11 +0900

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -27,14 +27,16 @@ ctest
 
 cd "$PREFIX/lib" || exit 1
 
-# for lib in $(ls libHepMC*); do
-#     base=$(basename "$lib")
-#     newname="libHepMC3${base#libHepMC}"
-#     ln -sf "$base" "$newname"
-# done
+for lib in $(ls libHepMC*); do
+    base=$(basename "$lib")
+    newname="libHepMC3${base#libHepMC}"
+    mv $base $newname
+    ln -sf "$newname" "$base"
+done
 
-# for lib in $(ls libHepMCfio*); do
-#     base=$(basename "$lib")
-#     newname="libHepMCfio3${base#libHepMCfio}"
-#     ln -sf "$base" "$newname"
-# done
+for lib in $(ls libHepMCfio*); do
+    base=$(basename "$lib")
+    newname="libHepMCfio3${base#libHepMCfio}"
+    mv $base $newname
+    ln -sf "$newname" "$base"
+done
