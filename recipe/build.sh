1ea6e2c6d5b17daa87128fa7cf1fd7f671b0176f
Marco Meyer-Conde　　　  メイヤーマコト <37673095+meiyasan@users.noreply.github.com>
2025-08-14 09:49:55 +0900

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,7 +5,10 @@ PYTHON_VERSION_NO_DOT=$(echo "$PYTHON_VERSION" | tr -d '.')
 
 SITEARCH=$(${PREFIX}/bin/python -c "import sysconfig; print(sysconfig.get_path('platlib'))")
 
-
+# @TODO: Introduce major env variable and adapt for HepMC2
+#CMake Error at CMakeLists.txt:26 (message):
+#  You must specify the momentum units with -Dmomentum:STRING=[MEV|GEV]
+  
 cmake -LAH -S source -B build \
     -DCMAKE_INSTALL_PREFIX=${PREFIX} \
     -DPYTHIA8_ROOT_DIR=${PREFIX} \
